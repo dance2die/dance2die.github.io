a0363f74e6a3ac7a617240836959f08aa6d56eef
Sung Kim <sungmkim80@gmail.com>
2018-02-09 23:34:16 -0500

    changed timeout to 500ms

--- dist/main-bundle.js
+++ dist/main-bundle.js
@@ -82,7 +82,7 @@ document.addEventListener("DOMContentLoaded", function () {
   image.onload = function () {
     setTimeout(function () {
       document.querySelector("#background").classList.add("background__loaded");
-    }, 1500);
+    }, 500);
   };
 
   var imageURL = "https://images.unsplash.com/photo-1444703686981-a3abbc4d4fe3?crop=entropy&dpr=2&fit=crop&fm=jpg&h=750&ixjsv=2.1.0&ixlib=rb-0.3.5&q=50&w=1450";
