5cb91a75b6221733da90a3d182ee59affb40ffcf
Sung Kim <sungmkim80@gmail.com>
2018-02-11 14:33:40 -0500

    Removed div wrapper and changes background from 'body' tag

--- dist/main-bundle.js
+++ dist/main-bundle.js
@@ -74,23 +74,33 @@
 // https://stackoverflow.com/a/21814964/4035
 document.addEventListener("DOMContentLoaded", function () {
   var image = new Image();
-  image.onload = function () {
-    var background = document.querySelector("#background");
-    setTimeout(function () {
-      // FAKE the pulsing effect.
-      // Firefox keep on flashing so let's fake the flicker as a pulsing effect.
-      background.style.opacity = 0;
-
-      setTimeout(function () {
-        background.classList.add("background__loaded");
-        background.style.opacity = 1;
-      }, 500);
-    }, 500);
-  };
-
   var imageURL = "https://images.unsplash.com/photo-1444703686981-a3abbc4d4fe3?crop=entropy&dpr=2&fit=crop&fm=jpg&h=750&ixjsv=2.1.0&ixlib=rb-0.3.5&q=50&w=1450";
   var imageSrc = "url(" + imageURL + ")";
   image.src = imageURL;
+
+  image.onload = function () {
+    // const background = document.querySelector("#background");
+    setTimeout(function () {
+      var background = document.querySelector("body");
+      background.classList.remove("background__from");
+      background.classList.add("background__to");
+      background.style.backgroundImage = imageSrc;
+
+      // background.classList.add("background_meta");
+      // background.style.opacity = 1;
+    }, 500);
+
+    // setTimeout(() => {
+    //   // FAKE the pulsing effect.
+    //   // Firefox keep on flashing so let's fake the flicker as a pulsing effect.
+    //   // background.style.opacity = 0;
+
+    //   setTimeout(() => {
+    //     background.classList.add("background__loaded");
+    //     background.style.opacity = 1;
+    //   }, 500);
+    // }, 500);
+  };
 });
 
 /***/ })
--- js/index.js
+++ js/index.js
@@ -2,21 +2,32 @@
 // https://stackoverflow.com/a/21814964/4035
 document.addEventListener("DOMContentLoaded", function() {
   let image = new Image();
-  image.onload = function() {
-    const background = document.querySelector("#background");
-    setTimeout(() => {
-      // FAKE the pulsing effect.
-      // Firefox keep on flashing so let's fake the flicker as a pulsing effect.
-      background.style.opacity = 0;
-      
-      setTimeout(() => {
-        background.classList.add("background__loaded");
-        background.style.opacity = 1;
-      }, 500);
-    }, 500);
-  };
-
   const imageURL = `https://images.unsplash.com/photo-1444703686981-a3abbc4d4fe3?crop=entropy&dpr=2&fit=crop&fm=jpg&h=750&ixjsv=2.1.0&ixlib=rb-0.3.5&q=50&w=1450`;
   const imageSrc = `url(${imageURL})`;
   image.src = imageURL;
+
+  image.onload = function() {
+    // const background = document.querySelector("#background");
+    setTimeout(() => {
+      const background = document.querySelector("body");
+      background.classList.remove("background__from");
+      background.classList.add("background__to");
+      background.style.backgroundImage = imageSrc;
+
+      // background.classList.add("background_meta");
+      // background.style.opacity = 1;
+    }, 500);
+
+    // setTimeout(() => {
+    //   // FAKE the pulsing effect.
+    //   // Firefox keep on flashing so let's fake the flicker as a pulsing effect.
+    //   // background.style.opacity = 0;
+
+    //   setTimeout(() => {
+    //     background.classList.add("background__loaded");
+    //     background.style.opacity = 1;
+    //   }, 500);
+    // }, 500);
+  };
+
 });
